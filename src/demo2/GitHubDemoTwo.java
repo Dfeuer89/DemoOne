8fb31a16c9cbb579fc3cc7484fcee16a512f49c7
dfeue <dfeue@WorkSmart.home>
2021-03-23 16:56:04 -0400

    Added Comment

--- src/demo2/GitHubDemoTwo.java
+++ src/demo2/GitHubDemoTwo.java
@@ -5,7 +5,10 @@ public class GitHubDemoTwo {
 	public static void main (String [] args) {
 		System.out.println("My first gitHub comit....");
 		
-		
+	  System.out.println("I did it");	
 	}
+	
+	//you added new code and now you have to share with the team
+	
 
 }
